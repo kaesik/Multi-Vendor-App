b136724a964060442654a9a05e287cee35a42632
kaesik <108717713+kaesik@users.noreply.github.com>
2024-03-26 21:29:13 +0100

    added category list

---
+++ lib/views/categories/all_categories.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AllCategories extends StatelessWidget {
+  const AllCategories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('All Categories'),
+      ),
+      body: const Center(
+        child: Text('All Categories Page'),
+      ),
+    );
+  }
+}
