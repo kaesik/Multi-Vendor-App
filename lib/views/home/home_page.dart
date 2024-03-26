47ee3666f9eab2f4a50a1837aa350d7a0d2c7be7
kaesik <108717713+kaesik@users.noreply.github.com>
2024-03-26 21:29:27 +0100

    implemented category list and custom appbar

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:multi_vendor_app/views/home/widgets/category_list.dart';
+import 'package:multi_vendor_app/widgets/custom_appbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,11 +8,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(128),
+        child: CustomAppBar(),
       ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: Center(
+        child: Column(
+          children: [
+            Divider(
+              indent: 24,
+              endIndent: 24,
+              thickness: 1,
+              color:
+                  Theme.of(context).colorScheme.onBackground.withOpacity(0.1),
+            ),
+            CategoryList(),
+            Text('Home Page'),
+          ],
+        ),
       ),
     );
   }
